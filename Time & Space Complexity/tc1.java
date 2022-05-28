438b8051f78bb0207a0317cf32d789327de7bb3d
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-05-29 00:18:33 +0530

    :fire: tc1 added

--- Time & Space Complexity/tc1.java
+++ Time & Space Complexity/tc1.java
@@ -1,6 +1,8 @@
+
 // Program to get the nth fibonacci number after calculating the formula for it using linear reccurrence relation steps
 import java.util.Scanner;
-public class tc1{
+
+public class tc1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
@@ -8,9 +10,11 @@ public class tc1{
         scan.close();
 
     }
-    public static void getFib(int n){
-    //     formula that i got is (1.68^n)
-    long num = (long)Math.pow(1.68, n);
-     System.out.println(num);
+
+    public static void getFib(int n) {
+        // formula that i got is (given below)
+        int num = (int) (Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));//this is correct
+        // int num = (int) (Math.pow(1.61,n)/2.13);//this is approx answer
+        System.out.println(num);
     }
 }
