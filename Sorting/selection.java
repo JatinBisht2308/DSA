1b7d7f7eb5aa68495012a67339b8689c5493c8e3
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-06-01 20:39:21 +0530

    :fire: sorting added

---
+++ Sorting/selection.java
@@ -0,0 +1,20 @@
+
+// Selection sorting -> steps and rule
+public class selection {
+   public static void main(String[] args) {
+      int[] arr = { 14, 33, 27, 10, 35, 19, 42, 44 };
+      selectionSort(arr);
+      for (int i = 0; i < arr.length; i++) {
+         System.out.println(arr[i]);
+      }
+   }
+
+   public static void selectionSort(int[] arr) {
+      for (int i = 0; i < arr.length-2; i++) {
+         for (int j = i+1; j < arr.length-1; j++) {
+                         
+         }
+      }
+   }
+
+}
