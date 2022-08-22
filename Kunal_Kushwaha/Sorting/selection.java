d595ba9869911908a366bd13d991d13fd8682faa
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-08-22 23:41:40 +0530

    :fire: pattern1 done

--- Kunal_Kushwaha/Sorting/selection.java
+++ Kunal_Kushwaha/Sorting/selection.java
@@ -1,5 +1,4 @@
-
-// Selection sorting -> steps and rule
+//  Selection sorting -> steps and rule
 public class selection {
    public static void main(String[] args) {
       int[] arr = { 14, 33, 27, 10, 35, 19, 42, 44 };
--- Striver _Sheet/Pattern's/pattern1.java
+++ Striver _Sheet/Pattern's/pattern1.java
@@ -1,5 +1,20 @@
+// Pattern 1-> 
+// *****
+// *****
+// *****
+// *****
+// *****
+import java.util.*;
+
+import javax.annotation.processing.SupportedOptions;
 public class pattern1 {
     public static void main(String[] args) {
-        System.out.println("hi i am pattern 1");
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<=5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
