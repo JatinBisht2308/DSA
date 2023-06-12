5406b414f97f7b1f56c04fc598a158df96f435b8
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-06-12 22:15:12 +0530

    print number 1 to n stack building method

--- Kunal_Kushwaha/Recursion/src/Rec1.java
+++ Kunal_Kushwaha/Recursion/src/Rec1.java
@@ -1,5 +1,22 @@
+
+import java.util.*;
+// Q1)- Print numbers form 1 to n using recursion.
 public class Rec1 {
     public static void main(String[] args) {
-        System.out.println(" HI recursion");
+        System.out.println("Recursion 1 question");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the number till u want to print the digits:");
+        int n = scan.nextInt();
+        oneToN(n,1);
+    }
+    public static void oneToN(int n,int s)
+    {
+//         base condition
+        if(s>n)
+        {
+            return;
+        }
+        System.out.println(s);
+        oneToN(n,s+1);
     }
 }
---
+++ Technical_Training/recursion/src/q1.java
@@ -0,0 +1,5 @@
+public class q1 {
+    public static void main(String[] args) {
+        System.out.println("hi this is recursion question 1");
+    }
+}
