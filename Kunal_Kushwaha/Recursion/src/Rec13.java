4cd4ab0ffd9c021c35569a8c2e06b744d4a65b5c
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-06-27 03:37:26 +0530

    recursion question13

---
+++ Kunal_Kushwaha/Recursion/src/Rec13.java
@@ -0,0 +1,16 @@
+import java.util.*;
+// Binary search in rotated sorted array using recursion.
+public class Rec13 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+        int target = scan.nextInt();
+
+    }
+
+}
