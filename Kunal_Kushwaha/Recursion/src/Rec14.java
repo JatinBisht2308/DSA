b7344f82e30b1575df18e4cf5e2254a8ea9b9453
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-06-27 03:37:41 +0530

    recursion question14

---
+++ Kunal_Kushwaha/Recursion/src/Rec14.java
@@ -0,0 +1,11 @@
+import java.util.*;
+// Rope cutting problem : We have n as the length of the rope and a,b and c are the possible length of the piece
+// we have to cut the rope in the maximum number of pieces for eg:-
+// n=5, a=2, b=5, c=1  Answer -> 5(5 piece of length 1 i.e c)
+// n=23, a=12, b=9, c=11 Answer -> 2(12 and 11) that means there is only one way to cut the rope that is 12 and 11 will be 2 part
+// n=5, a=4, b=2, c=6 Answer -> -1 (no two piece can be selected to make the sum as 5) here we can not cut this rope.
+public class Rec14 {
+    public static void main(String[] args) {
+
+    }
+}
