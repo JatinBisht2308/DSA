cc2f02d9de3d018d4dc12638e07bc8d719c32b89
JatinBisht2308 <82670244+JatinBisht2308@users.noreply.github.com>
2023-11-26 08:06:50 +0000

    Created new folder as pepcoding learning day one code

---
+++ Pepcoding/Day1/q1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class q1{
+  public static void main(String[] args)
+  {
+    System.out.println("Question 1");
+    Scanner scan = new Scanner(System.in);
+    // int num = scan.nextInt();
+    // System.out.println(num);
+    String str = scan.next();
+    System.out.println(str);
+  }
+}
---
+++ Pepcoding/Day1/q2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+// Is a number prime question
+public class q2{
+  public static void main(String[] args)
+  {
+    
+  }
+}
