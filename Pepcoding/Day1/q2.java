47a48e150cd479b1d75437057360069d8748845b
JatinBisht2308 <82670244+JatinBisht2308@users.noreply.github.com>
2023-11-26 08:34:23 +0000

    q2: Is number prime?

--- Pepcoding/Day1/q2.java
+++ Pepcoding/Day1/q2.java
@@ -3,6 +3,22 @@ import java.util.*;
 public class q2{
   public static void main(String[] args)
   {
-    
+    Scanner scan = new Scanner(System.in);
+    int t = scan.nextInt();
+    for(int i=0;i<t;i++)
+      {
+        int n = scan.nextInt();
+        String ans = checkIsPrime(n);
+        System.out.println(ans);
+      }
+  }
+  public static String checkIsPrime(int num)
+  {
+    for(int i=2;i<=num/2;i++)
+      {
+        if(num%i==0)
+          return "Not Prime";
+      }
+    return "Prime";
   }
 }
